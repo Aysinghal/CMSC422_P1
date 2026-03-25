339baef04a6d99bebdddb4b2c448b405c867dc3d
Aysinghal <69439929+Aysinghal@users.noreply.github.com>
2026-03-24 23:21:58 -0400

    Initial commit

---
+++ simple_classifier.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class MostFrequentClassClassifier:
+    def __init__(self):
+        self.prediction = 0
+
+    def train(self, X, y):
+        """
+        TODO: Find the most frequent label in y and store it in self.prediction.
+        """
+        num_pos = np.sum(y == 1.0)
+        num_neg = np.sum(y == -1.0)
+        self.prediction = 1.0 if num_pos >= num_neg else -1.0
+
+    def predict(self, X):
+        """
+        TODO: Return a vector of predictions, all equal to self.prediction.
+        """
+        return np.full(X.shape[0], self.prediction)
